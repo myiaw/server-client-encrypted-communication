01dc50c66bcb362161b4841e8181166eeb25c2a0
m1a01 <82682849+myaaw0@users.noreply.github.com>
2022-11-07 01:49:49 +0100

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+
+namespace Client {
+    class Program {
+        //NEEDED CONSTANTS
+        const int port = 1236;
+        const string ip = "127.0.0.1";
+        const int msg_size = 1024;
+        const int head_size = 1;
+        const string key = "key";
+
+        static bool isRunning = true;
+
+             static string? Receive(NetworkStream stream) {
+            try {
+                //WE RECIEVE MESSAGE IN BYTES (ARRAY OF BYTES WITH SIZE I SET EARLIER)
+                byte[] recievedBytes = new byte[msg_size];
+                int len = stream.Read(recievedBytes, 0, recievedBytes.Length);
+                //IF Read() IS NULL IT TRIGGERS EXCEPTION
+                return Encoding.UTF8.GetString(recievedBytes, 0, len);
+            }
+            catch (Exception e) {
+                Console.WriteLine(value: e.Message + "\n");
+                return null;
+            }
+        }
+        static void Send(NetworkStream stream, string msg) {
+            try {
+                byte[] sendMsg = Encoding.UTF8.GetBytes(msg.ToCharArray(), 0, msg.ToCharArray().Length);
+                stream.Write(sendMsg, 0, sendMsg.Length);
+            }
+            catch (Exception e) {
+                Console.WriteLine(value: e.Message + "\n");
+            }
+        }
+
+
+
+        static string Decrypt(string toDecrypt) {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            tdes.Key = md5.ComputeHash(Encoding.UTF8.GetBytes(key));     //kljuc sifre
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+            ICryptoTransform decryptor = tdes.CreateDecryptor();
+            byte[] datadsa = Convert.FromBase64String(toDecrypt);
+            byte[] shown = decryptor.TransformFinalBlock(datadsa, 0, datadsa.Length);
+            return Encoding.UTF8.GetString(shown);
+        }
+
+
+        static void Main(string[] args) {
+                Console.WriteLine("\nClient:");
+                while (true) {
+                    Console.WriteLine("\nEnter command: ");
+                    string input = Console.ReadLine();
+                    if (input == "q") {
+                        break;
+                    }
+                    try {
+                        TcpClient client = new TcpClient();
+                        client.Connect(ip, port);
+
+                        NetworkStream stream = client.GetStream();
+                        Send(stream, input);
+                        string answer = Receive(stream);
+
+                    if (input[0] == 'G') {
+
+                        answer += " --DECRYPTING--> " + Decrypt(answer);
+                    }
+                     
+                        Console.WriteLine("Client answer: " + answer);
+                    }
+                    catch (Exception e) {
+                        Console.WriteLine(value: e.Message + "\n");
+                    }
+                }
+                Console.WriteLine("\nPress any key to exit.");
+                Console.ReadKey();
+
+
+            }
+        }
+    }
---
+++ ComputerNetworks_V2-serverClient/Program.cs
@@ -0,0 +1,125 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+
+namespace Server {
+    class Program {
+        //NEEDED CONSTANTS
+        const int port = 1236;
+        const string ip = "127.0.0.1";
+        const int msg_size = 1024;
+        const int head_size = 1;
+        const string key = "key";
+
+        static bool isRunning = true;
+
+        static string? Receive(NetworkStream stream) {
+            try {
+                //WE RECIEVE MESSAGE IN BYTES (ARRAY OF BYTES WITH SIZE I SET EARLIER)
+                byte[] recievedBytes = new byte[msg_size];
+                int len = stream.Read(recievedBytes, 0, recievedBytes.Length);
+                //IF Read() IS NULL IT TRIGGERS EXCEPTION
+                return Encoding.UTF8.GetString(recievedBytes, 0, len);
+            }
+            catch (Exception e) {
+                Console.WriteLine(value: e.Message + "\n");
+                return null;
+            }
+        }
+        static void Send(NetworkStream stream, string msg) {
+            try {
+                byte[] sendMsg = Encoding.UTF8.GetBytes(msg.ToCharArray(), 0, msg.ToCharArray().Length);
+                stream.Write(sendMsg, 0, sendMsg.Length);
+            }
+            catch (Exception e) {
+                Console.WriteLine(value: e.Message + "\n");
+            }
+        }
+
+
+        static string Encrypt(string toEncrypt) {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            UTF8Encoding utf8 = new UTF8Encoding();
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            tdes.Key = md5.ComputeHash(Encoding.UTF8.GetBytes(key));     //kljuc sifre
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+            ICryptoTransform encryptor = tdes.CreateEncryptor();
+            byte[] datadsa = Encoding.ASCII.GetBytes(toEncrypt);
+            byte[] hidden = encryptor.TransformFinalBlock(datadsa, 0, datadsa.Length);
+            Console.WriteLine("Uspesno sifriranje.");
+
+            return Convert.ToBase64String(hidden, 0, hidden.Length);
+        }
+    
+    
+
+        static void Main(string[] args) {
+            TcpListener listener = new TcpListener(IPAddress.Parse(ip), port);
+            listener.Start();
+
+            Console.WriteLine("\nListening at: " + ip + ":" + port.ToString());
+            while (isRunning) {
+                using (TcpClient client = listener.AcceptTcpClient())
+                using (NetworkStream stream = client.GetStream()) {
+                    Console.WriteLine("\nClient has connected with: " + client.Client.RemoteEndPoint.ToString());
+                    string msg = Receive(stream);
+
+
+
+                    Console.WriteLine("\nRecieved:" + msg);
+                    string answer = "";
+
+                    string head = msg[0].ToString();
+                    string body = " ";
+
+                    if(msg.Length > 1) {
+                        body = msg.Substring(head_size, msg.Length - 1);
+                        if (body[0] == ' ') {
+                            body.Remove(0, 1);
+                        }
+
+                    }
+
+               
+
+
+                    switch (head) {
+                        case "A":
+                            answer = "Hello, " + client.Client.RemoteEndPoint.ToString();    //IP AND PORT
+                            break;
+                        case "B":
+                            answer = "Current time is: " + DateTime.Now.ToString("dd. MM. yyyy, H : mm"); //CURRENT DATE & TIME
+                            break;
+                        case "C":
+                            answer = Environment.CurrentDirectory; // CURRENT DIRECTORY
+                            break;
+                        case "D": 
+                            answer = body; //OUTPUT JUST THE BODY
+                            break;
+                        case "E":
+                            answer =  Environment.MachineName + "\n" + Environment.OSVersion.ToString();    //PC NAME AND OS VERSION
+                            break;
+                        case "G":
+                            answer = Encrypt(body);
+                            break;
+                    }
+
+                    Send(stream, answer);
+                    Console.WriteLine("Answer: " + answer);
+                }
+                Console.WriteLine("\nClient disconnected.");
+            }
+            listener.Stop();
+            Console.WriteLine("\nServer stopped.");
+            Console.ReadKey();
+        }
+
+
+    }
+}
